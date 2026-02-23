bb571de58ad21c1039f7679b667fbaf8a9fe5a00
Sajna Nazeer K <sajna_nazeerk@comcast.com>
2026-02-23 07:05:06 +0000

    Added logging of version

---
+++ westeros-version.h
@@ -0,0 +1,30 @@
+/*
+ * If not stated otherwise in this file or this component's Licenses.txt file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2018 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef _WESTEROS_SINK_VERSION_H
+#define _WESTEROS_SINK_VERSION_H
+
+#define WESTEROS_SINK_VERSION_MAJOR (2)
+#define WESTEROS_SINK_VERSION_MINOR (0)
+#define WESTEROS_SINK_VERSION_PATCH (0)
+#define WESTEROS_SINK_VERSION WESTEROS_SINK_VERSION_MAJOR, WESTEROS_SINK_VERSION_MINOR, WESTEROS_SINK_VERSION_PATCH
+
+#define WESTEROS_SINK_VERSION_FMT "%d.%02d.%02d"
+
+#endif
+
