edcd4b424be9cee96f53d03db88843e66166aa17
sajnanazeer <sajnanazeerk@gmail.com>
2026-03-13 23:08:29 +0530

    Opensourcing the westeros repositories (#13)
    Reason for change : Add license related changes
    Test Procedure : None
    Priority : P1
    Risks : None
    Signed-off-by : Sajna_NazeerK@comcast.com
    
    Co-authored-by: Sajna Nazeer K <sajna_nazeerk@comcast.com>

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+# Copyright (C) 2016 RDK Management
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
